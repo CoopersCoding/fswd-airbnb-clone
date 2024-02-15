214cf159358d5b9011304725c810df797be05418
Brian <blcooper76@gmail.com>
2024-02-15 13:39:13 +0000

    stripe implementation

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
     resources :users, only: [:create]
     resources :sessions, only: %i[create destroy]
     resources :properties, only: [:index, :show]
+    resources :bookings, only: [:create]
+    resources :charges, only: [:create]
 
     get '/properties/:id/bookings' => 'bookings#get_property_bookings'
     get '/authenticated' => 'sessions#authenticated'
